9f8387382d20d44391bfd098ec0a42d34dcf77fc
nimatel <ivicazup@gmail.com>
2017-05-11 23:26:58 +0200

    adding elements to the todo app
    some html,css and js

---
+++ resources/js/main.js
@@ -0,0 +1,9 @@
+//user clicked on the add button
+// if there is any text in the time filed add the text to the todo list
+document.getElementById('add').addEventListener('click', function(){
+    var value = document.getElementById('item').value;
+    if (value){
+      
+    }
+
+});
